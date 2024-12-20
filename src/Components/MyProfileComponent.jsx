c19b613f2b39cbf995d13a6d69fb6478501440fd
krunalvegda02 <Krunal.vegda02@gmail.com>
2024-12-20 18:48:16 +0530

    bugs introduced

--- src/Components/MyProfileComponent.jsx
+++ src/Components/MyProfileComponent.jsx
@@ -8,17 +8,34 @@ import {
 } from "../index";
 import { useSelector } from "react-redux";
 import { EditOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom";
+import { message } from "antd";
 
-function MyProfileComponent() {
+function MyProfileComponent({ isUser = true }) {
   const userdata = useSelector((state) => state.userData);
   const [pageState, setPageState] = useState(1); // Default active menu: Videos
+
+  {
+    if (!isUser) {
+      axios
+        .get(`/api/v1/c/:username`)
+        .then((res) => {
+          console.log("API RESPONSE:", res);
+        })
+        .catch((err) => {
+          message.error("Channel fetching error", err);
+          console.log("API ERROR:", err);
+        });
+    }
+  }
 
   return (
     <div>
       {/* Header cover image */}
       <div className="h-[100px] bg-white w-full">
         <img
-          src="https://tse1.mm.bing.net/th?id=OIP.ExnS3_PBvo0jK-W75PxmEwHaEK&pid=Api&P=0&h=180"
+          src={isUser ? userdata.coverImage : res.data.data.coverImage}
+          // "https://tse1.mm.bing.net/th?id=OIP.ExnS3_PBvo0jK-W75PxmEwHaEK&pid=Api&P=0&h=180"
           alt="coverimage"
           className="object-cover h-36 w-full"
         />
@@ -27,12 +44,18 @@ function MyProfileComponent() {
       {/* Profile Section */}
       <div className="flex justify-between p-3">
         <div className="flex text-left">
-          <Avatar h={130} w={130} src={userdata.avatar} />
+          <Avatar
+            h={130}
+            w={130}
+            src={isUser ? userdata.avatar : res.data.data.avatar}
+          />
           <div className="flex-col pt-11 pl-4">
             <p className="text-white text-3xl pb-1 font-semibold">
-              {userdata.fullname}
+              {isUser ? userdata.fullname : res.data.data.fullname}
             </p>
-            <p className="text-gray-300 text-sm pb-0.5">@{userdata.username}</p>
+            <p className="text-gray-300 text-sm pb-0.5">
+              @{isUser ? userdata.username : res.data.data.username}
+            </p>
             <div className="flex">
               <p className="text-gray-300 text-sm">{100} Subscribers</p>
               <p className="px-2 text-gray-300 text-sm">●</p>
@@ -40,10 +63,12 @@ function MyProfileComponent() {
             </div>
           </div>
         </div>
-
-        <div className="flex justify-center bg-violet-400 mt-12 mr-5 h-9 pt-1 w-20 rounded-xl hover:bg-violet-500 hover:border duration-300">
-          <EditOutlined /> <p className="pl-2 font-medium text-lg">Edit</p>
-        </div>
+        <Link to="/settings">
+          {" "}
+          <div className="flex justify-center bg-violet-400 mt-12 mr-5 h-9 pt-1 w-20 rounded-xl hover:bg-violet-500 hover:border duration-300">
+            <EditOutlined /> <p className="pl-2 font-medium text-lg">Edit</p>
+          </div>
+        </Link>
       </div>
 
       {/* Menu Bar */}
@@ -75,7 +100,7 @@ function MyProfileComponent() {
       </div>
 
       {/* Page Content */}
-      <div className="mt-4">
+      <div className="mt-2">
         {pageState === 1 && <ProfileVideos />}
         {pageState === 2 && <ProfilePlaylist />}
         {pageState === 3 && <ProfileTweets />}
--- src/Components/profile-components/ProfileTweets.jsx
+++ src/Components/profile-components/ProfileTweets.jsx
@@ -1,19 +1,77 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Avatar, Container } from "../../index";
 import { useSelector } from "react-redux";
+import { LikeOutlined, DislikeOutlined } from "@ant-design/icons";
+import { message } from "antd";
+// import { Input } from "antd";
 
 function ProfileTweets() {
   const userid = useSelector((state) => state.userData._id);
-//   useEffect(() => {
-//     axios.get(`api/v1/tweets/user/${userid}`);
-//   });
+  const [tweets, setTweets] = useState(null);
+
+  // useEffect(() => {
+  //   axios.get("api/v1/tweets");
+  // }, []);
+
+  const saveTweet = () => {
+    // if (!tweet.trim()) {
+    //   message.warning("Tweet cannot be empty!");
+    //   return;
+    // }
+    // axios
+    //   .post(`/api/v1/tweets`, { userId: userid, content: tweet })
+    //   .then(() => {
+    //     message.success("Tweet posted successfully!");
+    //     setTweet(""); // Clear the input field
+    //   })
+    //   .catch((err) => {
+    //     message.error("Failed to post tweet!");
+    //     console.error("Tweet error:", err);
+    //   });
+  };
 
   return (
-    <Container>
-      <div>
-       <Avatar h={60} w={60}   />
+    <>
+      <div className="relative w-full border border-gray-300 mb-3 shadow-sm">
+        <input
+          name="tweet"
+          // value={tweet}
+          className="w-full h-16 p-2 pb-9 border text-lg text-white border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-violet-500 bg-slate-700 pr-16"
+          placeholder="Write an Announcement..."
+        />
+        <button
+          className="absolute bottom-2 right-2 h-8 bg-violet-500 hover:bg-red-600 text-white px-4 py-1 shadow-md text-sm rounded"
+          onClick={saveTweet}
+        >
+          Save
+        </button>
       </div>
-    </Container>
+
+      <div className="flex ml-2 ">
+        <Avatar
+          h={50}
+          w={50}
+          src={
+            "https://static.vecteezy.com/system/resources/previews/018/765/757/original/user-profile-icon-in-flat-style-member-avatar-illustration-on-isolated-background-human-permission-sign-business-concept-vector.jpg"
+          }
+        />
+        <div className="flex-col text-white pl-3 text-left">
+          <div className="flex items-center">
+            <p className="text-lg">username</p>
+            <p className="text-gray-400 text-xs ml-4  ">20/11/23</p>
+          </div>
+          <p className="text-base">
+            Content Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Inventore, sequi.
+          </p>
+          <div className="mt-1">
+            <LikeOutlined className="pr-1" />
+            {"65"} <DislikeOutlined className="pr-1 pl-2" />
+            {"34"}
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
